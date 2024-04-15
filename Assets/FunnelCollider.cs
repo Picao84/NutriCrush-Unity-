7bd2b69a70de0e15b8bed81acb5ee546becfe28d
Tiago Picão <tiagopicao@outlook.com>
2024-04-15 16:40:36 +0100

    Tweaked dragging to increase the challenge

--- Assets/FunnelCollider.cs
+++ Assets/FunnelCollider.cs
@@ -21,7 +21,7 @@ public class FunnelCollider : MonoBehaviour
         if (other.gameObject.GetComponent<Sphere>() != null! && !other.gameObject.GetComponent<Sphere>().isPicked && other.gameObject.GetComponent<Sphere>().canBeAbsorbed)
         {
             other.gameObject.GetComponent<Sphere>().ResumeRotation();
-            other.gameObject.GetComponent<Rigidbody>().drag =8;
+            other.gameObject.GetComponent<Rigidbody>().drag =5.5f;
         }
     }
 
@@ -31,7 +31,7 @@ public class FunnelCollider : MonoBehaviour
         if (other.gameObject.GetComponent<Sphere>() != null! && !other.gameObject.GetComponent<Sphere>().isPicked && other.gameObject.GetComponent<Sphere>().canBeAbsorbed)
         {
             other.gameObject.GetComponent<Sphere>().ResumeRotation();
-            other.gameObject.GetComponent<Rigidbody>().drag = 8;
+            other.gameObject.GetComponent<Rigidbody>().drag = 5.5f;
         }
     }
 
